df9f3342724ebbe48cfb11a83b30781bd774a5a1
Giovanni Poblete <giovannipoblete55@gmail.com>
2023-05-19 06:26:50 -0400

    un vio

--- src/component/navbar/navlist/NavList.jsx
+++ src/component/navbar/navlist/NavList.jsx
@@ -8,25 +8,40 @@ import {useRef,} from "react";
 const NavList = ({ classType }) => {
     const dispatch = useDispatch()
     const { items } = useSelector(state => state.navBar)
+    const divRef = useRef(null);
 
-    const onMouseOverItemNavBar = (children) => {
-        console.log(children);
 
-        dispatch(DROPDOWN_ACTIONS.dropDownGet(children?.children))
-        dispatch(DROPDOWN_ACTIONS.toogleDropDown(children.name))
-        dispatch(DROPDOWN_ACTIONS.setSelectedDropDown(!children.dropDownIsShow))
-
+    const handleMouseEnter =(event, e) => {
+        dispatch(DROPDOWN_ACTIONS.toogleDropDown(e))
     }
-    console.log(items);
+
+    function handleMouseLeave(event, e) {
+        const {clientY } = event;
+        const divRect = divRef.current.getBoundingClientRect();
+        const roundedY = Math.round(divRect.bottom);
+        const roundedYc = Math.ceil(divRect.bottom);
+        const roundedYf = Math.floor(divRect.bottom);
+        if (clientY > roundedY || clientY > roundedYc || clientY > roundedYf) {
+              dispatch(DROPDOWN_ACTIONS.toogleDropDown(e))
+        }else{
+            dispatch(DROPDOWN_ACTIONS.toogleDropDown([]))
+        }
+    }
 
     return items.length !== 0  && items.navBar !== undefined ? (
         <>
             {
                 items.navBar.filter(item => item.type === classType).map(e => {
                     return (
-                        <ul key={e.id + classType} className={`navBar__container__${e.type}`}>
-                            <li key={e.id} className={`navBar__container__${e.type}__item`} name={e.name} onMouseOver={()=>{onMouseOverItemNavBar(e)}}>
+                        <ul        key={e.id + classType} className={`navBar__container__${e.type}`}>
+                            <li  key={e.id} className={`navBar__container__${e.type}__item`}
+                                name={e.name}
+                                onMouseEnter={(event)=>{handleMouseEnter(event,e)}}
+                               onMouseLeave={(event)=>{handleMouseLeave(event,e)}}
+
+                            >
                                 <Link
+                                    ref={divRef}
                                     className={`navBar__container__${e.type}__item__link`}
                                     to={e.to}>
                                     {e.name}
@@ -39,7 +54,7 @@ const NavList = ({ classType }) => {
                                 </Link>
                             </li>
                             {e?.children ?
-                                <DropDown  classType={e.type} children={e.children} />
+                                <DropDown parrent={e.name}  classType={e.type} children={e.children} />
                                 : undefined}
                         </ul>)
                 })
@@ -47,7 +62,7 @@ const NavList = ({ classType }) => {
             }
         </>
 
-    ) : <span>falta el cargando</span>;
+    ) : <span>Cargando barra de navegacion...</span>;
 }
 
 export default NavList;
