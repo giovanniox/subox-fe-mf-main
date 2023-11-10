d996c6127f1a9dd96de64a9756279a07ec666dd4
Giovanni Poblete <giovanni.poblete@agibiz.cl>
2023-11-10 09:53:10 -0300

    home

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,75 +1,98 @@
-import React from 'react';
+import React from "react";
 import "./home.scss"
-import iconNiños from './niños.png'
+import iconNiños from "./niños.png";
 
-import iconWoman from './woman.png'
-import iconMen from './men.png'
-import iconTemp from './temp.png'
-import iconOfer from './ofer.png'
+import iconWoman from "./woman.png";
+import iconMen from "./men.png";
+import iconTemp from "./temp.png";
+import iconOfer from "./ofer.png";
 const Home = () => {
   return (
-    <div className='home'>
-      <div class="home__jumbotrom">
-        <div className='home__jumbotrom__container'>
+    <div className="home">
+      <div className="home__jumbotrom">
+        <div className="home__jumbotrom-container">
+          <div className="home__jumbotrom-sub">
+            <h1>Conoce la nueva forma de comprar ropa</h1>
+            <p>recibe 2 prendas al mes</p>
+            <h2>$7990</h2>
+            <button>Unirme Al Club</button>
+          </div>
+          <div className="home__jumbotrom-img">
+            <div>
+              <img
+                src={iconOfer}
+                className="home__categories-container-circle-img"
+                alt="Zapato Azul"
+              />
+            </div>
+          </div>
         </div>
       </div>
 
-
-      <div class="home__categories">
-        <div className='home__categories__container'>
-        <div>
-            <div className='home__categories__container__title'>
-              <span className='home__categories__container__text'>Ofertas</span>
-            </div>
-            <div className='home__categories__container__circle'>
-              <img src={iconOfer} className='home__categories__container__circle__img' alt="Zapato Azul" />
-            </div>
-          </div>
-        <div>
-            <div className='home__categories__container__title'>
-              <span className='home__categories__container__text'>Temporada</span>
-            </div>
-            <div className='home__categories__container__circle'>
-              <img src={iconTemp} className='home__categories__container__circle__img' alt="Zapato Azul" />
-            </div>
-          </div>
-
+      <div className="home__categories">
+        <div className="home__categories-container">
           <div>
-            <div className='home__categories__container__title'>
-              <span className='home__categories__container__text'>Mujer</span>
+            <div className="home__categories-title">
+              <span className="home__categories-text">Ofertas</span>
             </div>
-            <div className='home__categories__container__circle'>
-              <img src={iconWoman} className='home__categories__container__circle__img' alt="Zapato Azul" />
+            <div className="home__categories-circle">
+              <img
+                src={iconOfer}
+                className="home__categories-circle-img"
+                alt="Zapato Azul"
+              />
             </div>
           </div>
-
-
           <div>
-            <div className='home__categories__container__title'>
-              <span className='home__categories__container__text'>Niños</span>
+            <div className="home__categories-title">
+              <span className="home__categories-text">Temporada</span>
             </div>
-            <div className='home__categories__container__circle'>
-              <img src={iconNiños} className='home__categories__container__circle__img' alt="Zapato Azul" />
+            <div className="home__categories-circle">
+              <img
+                src={iconTemp}
+                className="home__categories-circle-img"
+                alt="Zapato Azul"
+              />
             </div>
           </div>
-
-        
-
           <div>
-            <div className='home__categories__container__title'>
-              <span className='home__categories__container__text'>Hombre</span>
+            <div className="home__categories-title">
+              <span className="home__categories-text">Mujer</span>
             </div>
-            <div className='home__categories__container__circle'>
-              <img src={iconMen} className='home__categories__container__circle__img' alt="Zapato Azul" />
+            <div className="home__categories-circle">
+              <img
+                src={iconWoman}
+                className="home__categories-circle-img"
+                alt="Zapato Azul"
+              />
             </div>
           </div>
-
-
-    
-
+          <div>
+            <div className="home__categories-title">
+              <span className="home__categories-text">Niños</span>
+            </div>
+            <div className="home__categories-circle">
+              <img
+                src={iconNiños}
+                className="home__categories-circle-img"
+                alt="Zapato Azul"
+              />
+            </div>
+          </div>
+          <div>
+            <div className="home__categories-title">
+              <span className="home__categories-text">Hombre</span>
+            </div>
+            <div className="home__categories-circle">
+              <img
+                src={iconMen}
+                className="home__categories-circle-img"
+                alt="Zapato Azul"
+              />
+            </div>
+          </div>
         </div>
       </div>
-
     </div>
   );
 };
