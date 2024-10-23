7d6a9db06349d1ed414cd726c6a93818c7659330
giovanni.poblete <giovanni.poblete@fractalia.es>
2024-10-23 11:30:38 -0300

    fix app.js imports

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import { Provider } from 'react-redux';
 import store from './app/redux/store';
 import AuthProvider from "./provider/AuthProvider";
 import { GoogleOAuthProvider } from '@react-oauth/google';
-const googleKey = "916830881278-fv5evrd2t14lm6pqrf2h9anm34ucr0v4.apps.googleusercontent.com";
 import CssBaseline from '@mui/material/CssBaseline';
 
+const googleKey = "916830881278-fv5evrd2t14lm6pqrf2h9anm34ucr0v4.apps.googleusercontent.com";
 const App = () => {
 
     return (
